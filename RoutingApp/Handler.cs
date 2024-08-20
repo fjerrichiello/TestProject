f1d4af9d20134440263c598000ba99cfc634995e
Frank Errichiello <fjerrichiello@gmail.com>
2024-08-20 12:56:19 -0500

    Update again

--- RoutingApp/Handler.cs
+++ RoutingApp/Handler.cs
@@ -1,12 +1,21 @@
 ﻿namespace RoutingApp;
 
-public class Handler(IRouterParameterFactory factory, IBookActivityRouter router)
+public class Handler(IRouterParameterFactory factory, IBookActivityRouterAsync router, IBookActivityRouter router2)
 {
     public async Task UseHandler(Command command)
     {
         var parameters = await factory.GetParameters(command);
 
         var operation = await router.GetOperation(parameters);
+
+        await operation.DoOperation();
+    }
+
+    public async Task UseHandler2(Command command)
+    {
+        var parameters = await factory.GetParameters(command);
+
+        var operation = router2.GetOperation(parameters);
 
         await operation.DoOperation();
     }
